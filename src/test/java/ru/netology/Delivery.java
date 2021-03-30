b4a46cf04e44b89f8ef8a82cf8e61fa1ad9b577c
Yuriy Nikolaev <warlokk83@gmail.com>
2021-03-30 20:27:40 +0400

    Tests updated

--- src/test/java/ru/netology/Delivery.java
+++ src/test/java/ru/netology/Delivery.java
@@ -10,7 +10,6 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.openqa.selenium.Keys.*;
 
 
@@ -34,15 +33,15 @@ public class Delivery {
         $("[data-test-id=name] [type=text]").setValue("Иванов Иван");
         $(".checkbox__box").click();
         $("div.form-field>[type=button]").submit();
-        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + newDate));
+
     }
 
     @Test
     void shouldUseDropDownCity() {
         open("http://localhost:9999");
         $$("[type=text]").first().sendKeys("а", "с");
-//        $(withText("Астрахань")).click();
-        $$(".menu-item").find(exactText("Астрахань")).click();
+        $(byText("Астрахань")).click();
         $("[placeholder='Дата встречи']").sendKeys(CONTROL + "a", DELETE);
         String newDate = plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[placeholder='Дата встречи']").setValue(newDate);
@@ -50,33 +49,35 @@ public class Delivery {
         $("[data-test-id=name] [type=text]").setValue("Иванов Иван");
         $(".checkbox__box").click();
         $("div.form-field>[type=button]").submit();
-        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + newDate));
+
     }
 
     @Test
     void shouldUseCalendar() {
         open("http://localhost:9999");
         $$("[type=text]").first().sendKeys("а", "с");
-//        $(withText("Астрахань")).click();
         $$(".menu-item").find(exactText("Астрахань")).click();
         $(".input__icon [type=button]").click();
-        LocalDate today = LocalDate.now();
-        LocalDate date = plusDays(7);
-        String day = Integer.toString(date.getDayOfMonth());
-        if ((date.getMonth()) != today.getMonth()) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateInCalendar =  $("[placeholder='Дата встречи']").getValue();
+        LocalDate date = LocalDate.parse(dateInCalendar, formatter);
+        LocalDate newDate = plusDays(7);
+        String day = Integer.toString(newDate.getDayOfMonth());
+
+        if ((date.getMonth()) != newDate.getMonth()) {
             $("[data-step='1']").click();
-            $$(".calendar__day").find(exactText(day)).click();
-        } else {
-            $$(".calendar__day").find(exactText(day)).click();
         }
-        String actual = $("[placeholder='Дата встречи']").getValue();
-        String expected = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        assertEquals(expected,actual);
+
+        $$(".calendar__day").find(exactText(day)).click();
+        String expected = newDate.format(formatter);
         $("[name=phone]").setValue("+79999999999");
         $("[data-test-id=name] [type=text]").setValue("Иванов Иван");
         $(".checkbox__box").click();
         $("div.form-field>[type=button]").submit();
-        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + expected));
+
     }
 
 }
